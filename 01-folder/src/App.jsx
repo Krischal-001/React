142ce5f3b97f5f5dab1d13974e779d26cf0dc8b3
Krischal-001 <your_email@example.comkrischallamichhane001@gmail.com>
2026-03-23 22:59:51 +0545

    Intial Reactjs setup with vite

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App =()=>{
+  return(
+<>
+      <div id='parent'>
+      <h1 id='child1'>APP</h1>
+      <h3 id='child2'>Hello</h3>
+    </div>
+    <div id="chacha">
+    <h5 id="child3">Wassup</h5>
+    </div>
+</>
+  )
+}
+export default App
